7d6025ca8fbce5c56b0d89208c4250c2b4b058ab
sofiapel <sofiacarlapellegrini@gmail.com>
2024-12-11 19:58:55 -0300

    feat(setup): add typescript to environment

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,12 @@
-import express from "express";
+import express, { json } from "express";
+import dotenv from "dotenv"
 
+dotenv.config()
 const app = express()
 
-const port  = "4444";
+const port  = process.env.PORT ||"4444";
+
+app.use(json())
 
 app.get("/hello",(req, res)=> {
     res.send("Hello desde el server")
