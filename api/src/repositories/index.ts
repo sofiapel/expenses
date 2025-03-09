d92eaf78c31e4edab6631f519151ae334883da31
sofiapel <sofiacarlapellegrini@gmail.com>
2025-03-09 19:25:29 -0300

    test(add console log): a

--- api/src/repositories/index.ts
+++ api/src/repositories/index.ts
@@ -2,6 +2,7 @@ import { Sequelize } from 'sequelize';
 import dotenv from "dotenv"
 dotenv.config()
 
+console.log("ENVS: ",process.env.DB_NAME, process.env.DB_USER,  process.env.DB_PASSWORD)
 
 export const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASSWORD, {
   host: 'localhost',
